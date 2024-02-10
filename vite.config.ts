eb47367e07df3e9d6e9fe4c9b9d3e14371d9d13d
Mahesh Bhalerao <mahesh@binaryhat.in>
2024-02-10 10:33:35 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: "/my-digital-profile",
-  },
+  base: "/my-digital-profile/",
 });
